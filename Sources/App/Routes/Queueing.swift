fffef224e8ad326674c6b12ef1ab62b0b9f9a3f0
Alex Davis <git@alextdavis.me>
2017-06-15 12:45:52 -0400

    Fixed parameters in queueing

--- Sources/App/Routes/Queueing.swift
+++ Sources/App/Routes/Queueing.swift
@@ -3,19 +3,19 @@ import HTTP
 
 class QueueingRoutes: Routes {
     override func build(_ builder: RouteBuilder) throws {
-        builder.post("q", "dispatch", String.parameter) { req in //TODO: Switch this back to a post parameter
-            let message = try req.parameters.next(String.self)
+        builder.post("q", "dispatch") { req in
+            let message = req.data["message"] ?? ""
             switch message {
             case "next":
                 Queuer.q.next()
             case "prev":
                 Queuer.q.previous()
             case "enqueue_now":
-                Queuer.q.enqueueNow((req.parameters["id"]?.int)!)
+                Queuer.q.enqueueNow((req.data["id"]?.int)!)
             case "enqueue_next":
-                Queuer.q.enqueueNext((req.parameters["id"]?.int)!)
+                Queuer.q.enqueueNext((req.data["id"]?.int)!)
             case "enqueue_append":
-                Queuer.q.enqueueAppend((req.parameters["id"]?.int)!)
+                Queuer.q.enqueueAppend((req.data["id"]?.int)!)
             case "shuffle":
                 Queuer.q.shuffle()
             case "repeat":
