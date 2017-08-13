59e5de252310cf394c1d72af32e1832a795dc0d9
Alex T. Davis <appletreker@me.com>
2017-08-13 01:08:08 -0400

    reformatted Package, added dependencies to CMake

--- Package.swift
+++ Package.swift
@@ -3,26 +3,26 @@ import PackageDescription
 let package = Package(
         name: "valence-server",
         targets: [
-                Target(name: "App"),
-                Target(name: "Run", dependencies: ["App"]),
-                Target(name: "Ingest", dependencies: ["App"]),
+            Target(name: "App"),
+            Target(name: "Run", dependencies: ["App"]),
+            Target(name: "Ingest", dependencies: ["App"]),
         ],
         dependencies: [
-                .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
-                .Package(url: "https://github.com/vapor/fluent-provider.git", majorVersion: 1),
-                .Package(url: "https://github.com/vapor-community/postgresql-provider.git", majorVersion: 2),
-                .Package(url: "https://github.com/crossroadlabs/Regex.git", majorVersion: 1),
-                .Package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", majorVersion: 0),
-                .Package(url: "https://github.com/alextdavis/tilt-provider", majorVersion: 0),
-                .Package(url: "https://github.com/jkandzi/Progress.swift", majorVersion: 0),
+            .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
+            .Package(url: "https://github.com/vapor/fluent-provider.git", majorVersion: 1),
+            .Package(url: "https://github.com/vapor-community/postgresql-provider.git", majorVersion: 2),
+            .Package(url: "https://github.com/crossroadlabs/Regex.git", majorVersion: 1),
+            .Package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", majorVersion: 0),
+            .Package(url: "https://github.com/alextdavis/tilt-provider.git", majorVersion: 0),
+            .Package(url: "https://github.com/jkandzi/Progress.swift", majorVersion: 0),
 
         ],
         exclude: [
-                "Config",
-                "Database",
-                "Localization",
-                "Public",
-                "Resources",
+            "Config",
+            "Database",
+            "Localization",
+            "Public",
+            "Resources",
         ]
 )
 
