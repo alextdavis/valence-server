fb67413f96e48c3fd0b1cfca7d784ffef0670fb0
Alex T. Davis <appletreker@me.com>
2017-08-15 13:33:10 -0400

    Initial tinkering with Typeahead

--- Sources/App/Routes.swift
+++ Sources/App/Routes.swift
@@ -12,9 +12,29 @@ class Routes: RouteCollection {
             return try self.view.make("home.erb", ["layout": false])
         }
 
+        builder.post("search") { req in
+            try Search(try String(node: req.data["q"]))
+            return "hi"
+        }
+
         builder.group("typeahead") { g in
+            g.get("test") { req in
+                return try self.view.make("searchbox.erb")
+            }
+
             g.get("albums") { req in
-                
+                return try String(node: Song.database!.raw("SELECT array_to_json(array_agg(row_to_json(a))) FROM " +
+                        "(SELECT id, name FROM albums) a")[0]?["array_to_json"])
+            }
+
+            g.get("artists") { req in
+                return try String(node: Song.database!.raw("SELECT array_to_json(array_agg(row_to_json(a))) FROM " +
+                        "(SELECT id, name FROM artists) a")[0]?["array_to_json"])
+            }
+
+            g.get("tags") { req in
+                return try String(node: Song.database!.raw("SELECT array_to_json(array_agg(row_to_json(a))) FROM " +
+                        "(SELECT id, name FROM tags) a")[0]?["array_to_json"])
             }
         }
     }
